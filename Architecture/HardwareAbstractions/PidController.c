59bbefac13f66eb84bc9ad4bbf7bf900a008e433
Maximiliano Ronda <mronda11@gmail.com>
2018-09-19 18:58:34 -0400

    initial commit

---
+++ Architecture/HardwareAbstractions/PidController.c
@@ -0,0 +1,49 @@
+#include "msp.h"
+#include "PidController.h"
+#include "utils.h"
+
+int PidController_Run(PidController_t *instance, int goal)
+{
+    int pidOutput = 0;
+
+    pidOutput = (instance->kp * error) + (instance->ki * integral) + (instance->kd * derivative);
+
+    if(pidOutput < 0)
+    {
+//      forward = false;
+      pidOutput -= instance->basePidOutput;
+    }
+
+    if(pidOutput > 0)
+    {
+//      forward = true;
+      pidOutput += instance->basePidOutput;
+    }
+
+    if(pidOutput > instance->pidOutputCap)
+    {
+      pidOutput = 100;
+    }
+
+    if(pidOutput < -1*(instance->pidOutputCap))
+    {
+      pidOutput = -100;
+    }
+
+    return pidOutput;
+}
+
+void PidController_Init(
+    PidController_t *instance,
+    float kp,
+    float ki,
+    float kd,
+    uint8_t basePidOutput,
+    uint8_t pidOutputCap)
+{
+    instance->kp = kp;
+    instance->ki = ki;
+    instance->kd = kd;
+    instance->basePidOutput = basePidOutput;
+    instance->pidOutputCap = pidOutputCap;
+}
---
+++ Architecture/HardwareAbstractions/PidController.h
@@ -0,0 +1,28 @@
+#ifndef PIDCONTROLLER_H
+#define PIDCONTROLLER_H
+
+#include "PidController.h"
+#include "EventSubscriber_Synchronous.h"
+#include "I_Event.h"
+
+typedef struct
+{
+    float kp;
+    float ki;
+    float kd;
+    uint8_t basePidOutput;
+    uint8_t pidOutputCap;
+} PidController_t;
+
+/*
+ * Initializes pid controller
+ */
+void PidController_Init(
+    PidController_t instance
+    float kp,
+    float ki,
+    float kd,
+    uint8_t basePidOutput,
+    uint8_t pidOutputCap);
+
+#endif
